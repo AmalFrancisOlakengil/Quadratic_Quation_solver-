d300cfc399028a2a91b17ebf308e0c80c686876e
AmalFrancisOlakengil <143199482+AmalFrancisOlakengil@users.noreply.github.com>
2023-11-27 09:43:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def Determinant(a,b,c):
-    D = (b**2-4*a*c)**0.5
+    D = float((b**2-4*a*c)**0.5)
     return D
 def sol1(a,b,c):
     s1 = (-b+Determinant(a,b,c))/(2*a)
@@ -7,7 +7,9 @@ def sol1(a,b,c):
 def sol2(a,b,c):
      s2 = (-b-Determinant(a,b,c))/(2*a)
      return s2
-Eqn = input("Enter your equation(ax^2+bx+c): ")
-x1 = sol1(int(Eqn[0]),int(Eqn[5]),int(Eqn[8]))
-x2 = sol2(int(Eqn[0]),int(Eqn[5]),int(Eqn[8]))
-print("The solutions are %d and %d" %(x1,x2))
+print("For equation ax^2+bx+c")
+values = input("Enter the a,b and c values:")
+Eqn = values.split()
+x1 = sol1(int(Eqn[0]),int(Eqn[1]),int(Eqn[2]))
+x2 = sol2(int(Eqn[0]),int(Eqn[1]),int(Eqn[2]))
+print("The solutions are %f and %f" %(x1,x2))
